84d6be38349c5f029abe45684853e5aea09da551
Matt Murdock <mattmurdock9801@gmail.com>
2024-12-17 12:10:24 +0530

    Second changes text

--- app/page.js
+++ app/page.js
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card card-side bg-base-100 shadow-xl">
       <figure>
         <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
+          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+          alt="Movie"
         />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title">New movie is released!</h2>
+        <p>Click the button to watch on Jetflix app.</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary">Watch</button>
         </div>
       </div>
     </div>
